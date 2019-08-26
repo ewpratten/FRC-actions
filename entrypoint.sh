6b9239557fc4c5930e5fd4a549112d964f65f478
Evan Pratten <ewpratten@gmail.com>
2019-08-26 11:12:50 -0400

    fix ops

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 
 chmod +x gradlew
 
-if [ "$1" == "build" ]; then
+if [ "$1" = "build" ]; then
     echo "Running only build task"
     ./gradlew build
 fi
 
-if [ "$1" == "unit_test" ]; then
+if [ "$1" = "unit_test" ]; then
     echo "Running only test task"
     ./gradlew test
 fi
 
-if [ "$1" == "all" ]; then
+if [ "$1" = "all" ]; then
     echo "Running all tasks"
     ./gradlew test
     ./gradlew build
