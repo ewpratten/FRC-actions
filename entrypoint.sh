32433aa725d8ed5efe4a40bc44d5a7a99763e643
Evan Pratten <ewpratten@gmail.com>
2020-06-11 13:50:19 -0400

    Starting work on simulation CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,3 +21,24 @@ if [ "$1" = "all" ]; then
     ./gradlew test
     ./gradlew build
 fi
+
+# if [ "$1" = "x_simulation" ]; then
+#     echo "Running simulation task [BETA]"
+#     echo "Simulation is not fully supported by @ewpratten. This exists for internal use only by team 5024."
+
+#     echo "Spawning GradleRIO simulation runner to generate sim scripts"
+#     ./gradlew simulateJava --console=plain
+#     task_pid=$(cat robot/build/pids/simulateJava.pid)
+#     kill -9 $task_pid
+#     echo "Simulation script has been generated. Killing simulator"
+
+#     echo "Waiting for sockets to close"
+#     sleep 2
+
+#     echo "Spawning simulator"
+#     timeout 10 ./robot/build/gradlerio_simulateJava.sh || EX=$?
+
+#     if ["$EX" != "124"]; then
+#         return 1
+#     fi
+# fi
--- test-entrypoint.sh
+++ test-entrypoint.sh
@@ -4,8 +4,11 @@ apt-get install git -y
 
 mkdir frc
 cd frc
-git clone https://github.com/frc5024/DeepSpace-SWI.git
-cd DeepSpace-SWI
+git clone https://github.com/frc5024/InfiniteOffseason.git repo
+
+cd repo
+
+git submodule update --init --recursive
 
 # Init gradle
 /opt/gradle/gradle-5.0/bin/gradle wrapper
